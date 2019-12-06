ffde715d821d7904a6036a82cc63bbf49bf5a1c8
Sunny <99049635@mydavinci.nl>
2019-12-06 15:42:54 +0100

    aanpassing js en image's toegevoegd

--- adventure.js
+++ adventure.js
@@ -23,56 +23,116 @@ function startGame(){
 		button3.setAttribute ("onclick","credits();")
 }
 function instructions(){
-	description.style.fontSize = "20px" 
-	description.innerHTML ='Instructies:<br>In deze game moet je ervoor zorgen dat je levend uit het huis komt!.';
-	description.style.textAlign = "center"
-	description.style.margin = "10px"
+	description.style.fontSize = "20px" ;
+	description.innerHTML ='Instructies:<br>In deze game moet je ervoor zorgen dat je levend uit het huis komt!<br>Door items te zoeken en kijk goed uit want je bent niet alleen';
+	description.style.textAlign = "center";
+	description.style.margin = "10px";
 }
 
 function credits(){
 	description.style.fontSize = "40px" 
 	description.innerHTML ='Gemaakt door: <br> Sunny Tavares Delagdo. <br> Klas: <br> LPIAO19A3';
-	description.style.textAlign = "center"
-	description.style.margin = "150px"
+	description.style.textAlign = "center";
+	description.style.margin = "150px";
 }
 
 function level1(){
 	image.src = "img/hall.jpg";
-	title.style.display = "none"
-	description.innerHTML = "Maak een keuze uit de drie volgende doorgangen <br> Deur 1 <br> Deur 2 <br> Lift"
-	description.style.marginLeft = "170px"
-	button1.innerHTML = "deur 1"
-	button1.setAttribute("onclick", "level2();")
-	button1.style.marginLeft = "110px"
-	button2.innerHTML = "deur 2"
-	button2.setAttribute ("onclick","level3();")
-	button3.innerHTML = "Lift"
-	button3.setAttribute ("onclick","lift();")
-}
-
-function level2(){
-	image.src = "img/torture.jpg"
-	description.innerHTML = "Ga opzoek naar de lift sleutel als ik jou was zal ik goed rond kijken"
-	description.style.marginRight = "550px"
-	button1.innerHTML = "hall"
-	button1.setAttribute("onclick", "level1();")
-	button2.innerHTML = "In emmer kijken"
-	button3.style.display = "none"
-}
-
-function level3(){
-	image.src = "img/room.jpg"
-	button1.innerHTML = "hall"
-	button1.setAttribute("onclick", "level1();")
-	button2.style.display = "none"
-	button3.style.display = "none"
+	title.style.display = "none";
+	description.innerHTML = "Maak een keuze; uit de drie volgende doorgangen <br> 1. Deur 1 <br> 2. Deur 2 <br> 3. Lift";
+	description.style.marginLeft = "150px";
+	button1.innerHTML = "deur 1";
+	button1.setAttribute("onclick", "level2();");
+	button1.style.marginLeft = "100px";
+	button2.innerHTML = "deur 2";
+	button2.setAttribute ("onclick","level3();");
+	button2.style.display = "inline-block;"
+	button3.innerHTML = "Lift";
+	button3.setAttribute ("onclick","lift();");
+	button3.style.display = "inline-block";
 }
 
 function lift(){
 	if (key == false){
-	alert("Je hebt geen lift sleutel ga eerst opzoek naar de sleutel")
+	alert("Je hebt geen lift sleutel ga eerst opzoek naar de sleutel");
 	}
 }	
+
+function level2(){
+	image.src = "img/torture.jpg"
+	description.innerHTML = "Ga opzoek naar de lift sleutel als ik jou was zal ik goed rond kijken";
+	description.style.marginRight = "540px";
+	button1.innerHTML = "hall";
+	button1.setAttribute("onclick", "level1();");
+	button1.style.display = "inline-block";
+	button1.style.marginLeft = "120px";
+	button2.innerHTML = "In emmer kijken";
+	button2.setAttribute ("onclick","bucket();")
+	button3.style.display = "none";
+}
+function bucket(){
+	if (key == false){
+	image.src = "img/bucket-withkey.png";
+	description.style.display = "none";
+	button1.innerHTML = "oppakken";
+	button1.setAttribute("onclick","getkey()");
+
+}else{
+	image.src = "img/bucket.png";
+	description.innerHTML ="Je hebt nu de lift sleutel";
+	description.style.display = "inline-block";
+	button1.style.display = "none";
+	inventoryItem.style.display = "inline-block";
+    inventoryItem.src = "img/key.jpg";
+    inventoryItem.style.width = "200px";
+    inventoryItem.style.height = "75px";
+    inventoryItem.style.marginLeft = "20px";
+    button2.innerHTML = "kijk uit het emmer";
+ 	button2.setAttribute("onclick", "level2a()");
+	button3.style.display = "none";
+}
+}
+
+function getkey(){
+    key = true;
+    bucket();
+}
+function level2a(){
+	image.src = "img/torture.jpg";
+	description.innerHTML = "Ga opzoek in de andere kamer";
+	button1.innerHTML = "hall";
+	button1.setAttribute("onclick", "level1();");
+	button1.style.display = "inline-block";
+	button1.style.marginLeft = "120px";
+	button2.innerHTML = "In emmer kijken";
+	button2.setAttribute ("onclick","bucket();");
+	button3.style.display = "none";
+}
+
+function level3(){
+	image.src = "img/room.jpg";
+	description.innerHTML = "Door zoek de kamer";
+	description.style.marginLeft = "120px";
+	button1.style.marginLeft = "25px";
+	button1.innerHTML = "hall"
+	button1.setAttribute("onclick", "level1();");
+	button1.style.display = "inline-block";
+	button2.innerHTML = "Kijk onder het bed";
+	button2.setAttribute("onclick", "bed();");
+	button2.style.display = "inline-block";
+	button3.innerHTML = "Kijk in de kast";
+	button1.setAttribute("onclick", "kast();");
+	button3.style.display = "inline-block";
+}
+function bed(){
+	image.src = "img/monster.jpg";
+	description.innerHTML= "Game over"
+	button1.innerHTML = "Start over"
+	button2.style.display = "none"
+	button3.style.display = "none"
+}
+
+
 
 
 
